f246c5b5a72c55b08bdbdbcd5318f56e87419ce8
Alejandro <Alejandro@172.16.100.20>
2017-01-25 13:04:09 +0100

    save

--- src/ejercicio1.java
+++ src/ejercicio1.java
@@ -1,9 +1,34 @@
+import java.util.Scanner;
 
 public class ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
+		float gradoscentigrados = 0.0f;
+		float resultado = 0.0f;
+		String aux = "";
+		boolean error = false;
 
+		do {
+			error = false;
+			System.out.print("Introduzca lo grados centigrados: ");
+			try {
+				aux = teclado.nextLine();
+				gradoscentigrados = Float.parseFloat(aux);
+			} catch (Exception e) {
+				error = true;
+				System.out.println("Error, No has introducido un número");
+			}
+		} while (error);
+		resultado = conversión(gradoscentigrados);
+		System.out.print("Los grados fahrenheit convertidos de los grados centigrados son: " + resultado + "ºF");
+
+	}
+
+	public static float conversión(float gradoscentigrados) {
+		float gradofahrenheit = 0.0f;
+		gradofahrenheit = 32 + (9 * gradoscentigrados / 5);
+		return gradofahrenheit;
 	}
 
 }
